5305e3eeda7094dae1cb777a70574976757ecf1a
In-Lay <flamefreeme@gmail.com>
2023-08-29 13:51:41 +0300

    Some bug fixes

---
+++ app/src/test/java/com/inlay/hotelroomservice/presentation/viewmodels/settings/SettingsViewModelTest.kt
@@ -0,0 +1,36 @@
+package com.inlay.hotelroomservice.presentation.viewmodels.settings
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class SettingsViewModelTest {
+
+    @Test
+    fun getLangsList() {
+    }
+
+    @Test
+    fun getInAppNotificationsState() {
+    }
+
+    @Test
+    fun initialize() {
+    }
+
+    @Test
+    fun openLangDialog() {
+    }
+
+    @Test
+    fun changeNotificationsState() {
+    }
+
+    @Test
+    fun onDarkModeChanged() {
+    }
+
+    @Test
+    fun onNotificationsSwitchChanged() {
+    }
+}
